9fa8b3b6bdc306b1d3aa925bbc0783a741b2bac5
Pedro de Castro Kurtz <pedrokurtz.01@outlook.com>
2026-02-02 18:45:15 -0300

    commit inicial

---
+++ Aula02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Diagnostics;
+using System.Reflection.Emit;
+
+namespace Aula02;
+
+public class Program
+{
+    public static void Main()
+    {
+        string myName = "Antonio"; // Variavel palavras
+        int number = 20; // Variavel numeros inteiros
+        double numberDouble = 10.5; // Variavel numeros reais
+        char character = 'a'; // Variavel caracteres
+        bool boolean = true; // Variavel Booleana
+
+        Console.WriteLine(myName);
+        Console.WriteLine(number);
+        Console.WriteLine(numberDouble);
+        Console.WriteLine(character);
+        Console.WriteLine(boolean);
+    }
+}
---
+++ Aula03/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Aula03;
+
+public class Program
+{
+    public static void Main()
+    {
+        int number = 30;
+
+        number = 20;
+
+        Console.WriteLine("Valor da variavel é " + number);
+
+        const int number2 = 1;
+
+        Console.WriteLine("O valor da constante é " + number2);
+    }
+}
